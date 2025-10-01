f5752a154c7ac714f6069065f038c8a623a56cc8
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-01 14:36:46 +0100

    rem public

--- src/test/java/com/raxrot/StudentTest.java
+++ src/test/java/com/raxrot/StudentTest.java
@@ -8,12 +8,12 @@ class StudentTest {
     private Student ryan;
 
     @BeforeAll
-    public static void onceExecutedBeforeAll() {
+     static void onceExecutedBeforeAll() {
         System.out.println("Test Student Class");
     }
 
     @BeforeEach
-    public void setup() {
+     void setup() {
         fer = new Student(1L, "fer");
         fer.addGrade(100);
         fer.addGrade(80);
@@ -21,27 +21,27 @@ class StudentTest {
 
     @DisplayName("Check data in Student object")
     @Test
-    public void testCreateStudent() {
+     void testCreateStudent() {
         assertNull(ryan);
         assertNotNull(fer);
     }
 
     @Disabled("Dissable Test")
     @Test
-    public void testStudentFields() {
+     void testStudentFields() {
         assertEquals(1L, fer.getId());
         assertEquals("fer", fer.getName());
         assertEquals(2, fer.getGrades().size());
     }
 
     @Test
-    public void testAddGrade() {
+     void testAddGrade() {
         assertEquals(100, fer.getGrades().get(0));
         assertEquals(80, fer.getGrades().get(1));
     }
 
     @Test
-    public void testAverageGrade() {
+     void testAverageGrade() {
         assertEquals(90, fer.getGradeAverage(), 0.01);
     }
 }
